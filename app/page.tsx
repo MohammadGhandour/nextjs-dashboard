07539437aae84f48a6ccce2bef76376bda316a52
Mohammad Ghandour <mohammadghandour02@outlook.com>
2025-09-29 23:05:25 +0300

    testing metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,23 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
+import { Metadata, ResolvingMetadata } from "next";
+
+export async function generateMetadata(parent: ResolvingMetadata): Promise<Metadata> {
+  const stock = await fetch(`https://b197dbcc2041.ngrok-free.app/info`).then(res => res.json());
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: stock.name,
+    description: `Current stock price for ${stock.name} is $${stock.price}`,
+    openGraph: {
+      images: [stock.logo, ...previousImages],
+    },
+  }
+};
 
 export default function Page() {
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
