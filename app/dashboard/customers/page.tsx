b6d2bbfda3c89e4822ca0891706672d9ef3b68e1
Mohammad Ghandour <mohammadghandour02@outlook.com>
2025-09-29 21:53:45 +0300

    Added customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import Table from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Metadata } from "next";
 
-export default function page() {
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function page(props: { searchParams?: Promise<{ query?: string }> }) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
 
   return (
-    <p>Customers Page</p>
-  )
-};
+    <div className="w-full">
+      <Table customers={customers} />
+    </div>
+  );
+}
